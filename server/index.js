179aea7741440cd953018da702d31529e5e91b5b
TKM_KraSuang <nonmeehatchai@gmail.com>
2024-01-24 12:10:44 +0700

    Database and Server Prepare

--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
-const express = require('express')
-const vhost = require('vhost')
+const express = require('express');
+const cors = require('cors')
+const vhost = require('vhost');
 const dotenv = require('dotenv');
+const { connectToDB } = require('./src/config/db.config.js')
+
 
 dotenv.config();
 
-const app = express()
-const api = express()
+const app = express();
+const api = express();
 const port = process.env.PORT
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.use(cors());
+
+app.use(vhost('api.*', api))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
